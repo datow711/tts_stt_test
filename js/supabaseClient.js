dda8bbf873dbd6dee8959efc1244bb692b90e948
datow711 <datow711@gmail.com>
2025-09-25 23:51:23 +0800

    update supabase settings

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,7 +4,7 @@
 const { createClient } = supabase;
 
 const SUPABASE_URL = 'https://uoftjfcuwhlfunxgpoki.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvZnRqZmN1d2hsZnVueGdwb2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MTMxNjQsImV4cCI6MjA3NDM4OTE2NH0.kZL82PIhHzBR4J2BeEDdBWVF4-KQxONpMO7KBWQovM0';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvZnRqZmN1d2hsZnVueGdwb2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MTMxNjQsImV4cCI6MjA3NDM4OTE2NH0.kZL86PIhHzBR4J2BeEDdBWVF4-KQxONpMO7KBWQovM0';
 
 // Initialize the client and assign it to a new global variable `supabaseClient`
 // to be used by other scripts.
